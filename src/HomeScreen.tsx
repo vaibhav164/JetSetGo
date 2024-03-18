17b3be8b769971db1b4a7c24ab99104428c95991
vaibhav164 <vai1641996@gmail.com>
2024-03-18 21:12:47 +0530

    First Commmit

---
+++ src/HomeScreen.tsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import { StyleSheet, Text, View } from 'react-native'
+
+export const HomeScreen =()=>{
+    return(
+        <View style={styles.container}>
+            <Text>{"Welcome To Home Screen"}</Text>
+        </View>
+    )
+}
+
+const styles = StyleSheet.create({
+    container:{
+        justifyContent:'center',
+        alignItems:'center',
+        flex:1
+    }
+})
