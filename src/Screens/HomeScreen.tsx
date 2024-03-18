32b7e2e4da7468bdcae9832f6983d811d55c9a0c
vaibhav164 <vai1641996@gmail.com>
2024-03-19 02:14:59 +0530

    Added Booking Screen Navigation from Homescreen

--- src/Screens/HomeScreen.tsx
+++ src/Screens/HomeScreen.tsx
@@ -1,16 +1,20 @@
 import React from 'react'
 import { Image, StyleSheet, Text, View } from 'react-native'
+import Button from '../components/Buttons'
 
-export const HomeScreen =()=>{
+export const HomeScreen =({ navigation })=>{
     return(
         <View style={styles.container}>
-            <View style={{height:'30%', width:'100%', backgroundColor:'yellow', borderBottomEndRadius:30}}>
+            <View style={styles.imageBox}>
             <Image source={require('../assets/images/flight.gif')} style={styles.image}/>
             </View>
             <View style={styles.formBox}>
-                <View style={{height:'10%',width:'100%', backgroundColor:'#3199da', borderBottomEndRadius:100, borderBottomStartRadius:100}}></View>
-                
-            <Text style={{fontSize:20}}>{"Welcome To Home Screen"}</Text>
+                <View style={styles.textBox}>
+                <Text style={styles.text}>{"JetSetGo!"}</Text>
+                </View>
+                <View style={styles.buttonBox}>
+                <Button title='Start Booking' handleSubmit={()=>{navigation.navigate('BookingScreen')}}/>
+                </View>
             </View>
         </View>
     )
@@ -30,6 +34,30 @@ const styles = StyleSheet.create({
     formBox:{
         flex:1,
         alignItems:'center',
-        width:'100%'
-    }
+        width:'100%',
+    },
+    imageBox:{
+        height:'30%', 
+        width:'100%', 
+        backgroundColor:'yellow', 
+        borderBottomEndRadius:30
+    },
+    buttonBox:{
+        height:'100%',
+        width:'100%',
+        justifyContent:'center',
+        alignItems:'center'
+    },
+    textBox:{height:'10%',
+    width:'100%', 
+    backgroundColor:'', 
+    borderBottomEndRadius:100, 
+    borderBottomStartRadius:100, 
+    justifyContent:'center', 
+    alignItems:'center'
+},
+    text:{fontSize:22, 
+        fontWeight:'600', 
+    color:'#fff'
+}
 })
