7d0c1bdefa43e3e29ada4af08fffd6e3acc5b60c
vaibhav164 <vai1641996@gmail.com>
2024-03-19 02:14:40 +0530

    Added Booking Screen

--- src/Navigators/RootNavigator.tsx
+++ src/Navigators/RootNavigator.tsx
@@ -2,6 +2,7 @@ import React from 'react'
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import { HomeScreen } from '../Screens/HomeScreen';
+import { BookingScreen } from '../Screens/BookingScreen';
 
 export const RootNavigator = () => {
     const Stack = createNativeStackNavigator()
@@ -11,6 +12,7 @@ export const RootNavigator = () => {
                 headerShown:false
             }}>
                 <Stack.Screen name='HomeScreen' component={HomeScreen} />
+                <Stack.Screen name='BookingScreen' component={BookingScreen} />
             </Stack.Navigator>
         </NavigationContainer>
     )
---
+++ src/Screens/BookingScreen.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import { StyleSheet, View } from 'react-native'
+
+export const BookingScreen =()=>{
+    return(
+        <View style={styles.container}></View>
+    )
+}
+const styles = StyleSheet.create({
+    container:{
+        flex:1,
+        padding:'3%'
+    }
+})
